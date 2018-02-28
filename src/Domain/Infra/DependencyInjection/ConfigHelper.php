e9b3f8f3cf603c39d9e5a01f67a1d6dcbe9df8d8
Roland Franssen <franssen.roland@gmail.com>
2018-02-28 14:34:39 +0100

    cleanup

--- src/Domain/Infra/DependencyInjection/ConfigHelper.php
+++ src/Domain/Infra/DependencyInjection/ConfigHelper.php
@@ -16,65 +16,14 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  * @author Roland Franssen <franssen.roland@gmail.com>
  *
  * @internal
+ *
+ * @todo move to BundleHelper
  */
 final class ConfigHelper
 {
     public const DEFAULT_ID_TYPE = 'integer';
     public const UUID_TYPES = ['uuid', 'uuid_binary', 'uuid_binary_ordered_time'];
-    public const NATIVE_DATA_TYPES = ['string', 'integer', 'bigint'];
     public const UUID_DATA_TYPES = ['uuid', 'uuid_binary', 'uuid_binary_ordered_time'];
-
-    // @todo consider custom node type instead
-    public static function createClassMappingNode(string $name, array $required = [], array $abstracts = [], bool $valueIsClass = false, \Closure $normalizer = null, $defaultValue = null, string $prototype = 'scalar', \Closure $prototypeCallback = null, NodeBuilder $builder = null): ArrayNodeDefinition
-    {
-        $node = ($builder ?? new NodeBuilder())->arrayNode($name);
-        $node->useAttributeAsKey('class');
-
-        if ($required) {
-            $node->isRequired();
-
-            foreach ($required as $class) {
-                $node->validate()->ifTrue(function (array $value) use ($class) {
-                    return !isset($value[$class]);
-                })->thenInvalid(sprintf('Class "%s" must be configured.', $class));
-            }
-        }
-
-        if (null !== $normalizer) {
-            $node->beforeNormalization()->always($normalizer);
-        }
-
-        $prototype = $node->prototype($prototype);
-        $prototype->defaultValue($defaultValue);
-
-        if (null !== $prototypeCallback) {
-            $prototypeCallback($prototype);
-        }
-
-        $node->validate()->always(function (array $value) use ($abstracts, $valueIsClass): array {
-            foreach ($value as $class => $mappedClass) {
-                if (!($isClass = class_exists($class)) && !interface_exists($class)) {
-                    throw new \LogicException(sprintf('A class or interface named "%s" does not exists.', $class));
-                }
-
-                if (!$valueIsClass || null === $mappedClass || ($isClass && $mappedClass === $class)) {
-                    continue;
-                }
-
-                if (!class_exists($mappedClass)) {
-                    throw new \LogicException(sprintf('A class named "%s" does not exists.', $mappedClass));
-                }
-
-                if ((!$isClass || in_array($class, $abstracts, true) || (new \ReflectionClass($class))->isAbstract()) && !is_subclass_of($mappedClass, $class)) {
-                    throw new \LogicException(sprintf('The class "%s" must be a sub class of "%s".', $mappedClass, $class));
-                }
-            }
-
-            return $value;
-        });
-
-        return $node;
-    }
 
     public static function defaultBundleConfig(array $defaultIdClassMapping, array $idClassMappingPerType): \Closure
     {
@@ -89,7 +38,7 @@ final class ConfigHelper
                     }
 
                     if (null === $mappedClass = $idClassMapping[$type][$class] ?? $defaultIdClassMapping[$class] ?? null) {
-                        throw new \LogicException(sprintf('Unknown ID class "%s" detected in "id_type_mapping" configuration.', $class));
+                        $mappedClass = in_array($type, self::UUID_TYPES, true) ? UuidInfra\DomainId::class : DomainId::class;
                     }
 
                     $value['class_mapping'][$class] = $mappedClass;
@@ -106,55 +55,6 @@ final class ConfigHelper
 
             return $value;
         };
-    }
-
-    public static function resolveResolveDataTypeMapping(ContainerBuilder $container, array &$config): void
-    {
-        if (!$container->hasParameter('msgphp.default_data_type')) {
-            $container->setParameter('msgphp.default_data_type', 'integer');
-        }
-
-        foreach ($config as &$value) {
-            $value = $container->getParameterBag()->resolveValue($value ?? '%msgphp.default_data_type%');
-        }
-
-        unset($value, $config);
-    }
-
-    public static function resolveClassMapping(array $dataTypeMap, array $dataTypeMapping, array &$config): void
-    {
-        foreach ($config as $key => &$value) {
-            if (null !== $value) {
-                continue;
-            }
-
-            if (!isset($dataTypeMapping[$key])) {
-                $value = $key;
-                continue;
-            }
-
-            $value = DomainId::class;
-            if (in_array($dataType = $dataTypeMapping[$key], self::UUID_DATA_TYPES, true)) {
-                if (!interface_exists(UuidInterface::class)) {
-                    throw new \LogicException(sprintf('Data type "%s" for identifier "%s" requires "ramsey/uuid".', $dataType, $key));
-                }
-
-                $value = UuidInfra\DomainId::class;
-            }
-
-            if (!isset($dataTypeMap[$key])) {
-                continue;
-            }
-
-            foreach ($dataTypeMap[$key] as $class => $dataTypes) {
-                if (in_array($dataType, $dataTypes, true)) {
-                    $value = $class;
-                    break;
-                }
-            }
-        }
-
-        unset($value, $config);
     }
 
     public static function resolveCommandMapping(array $classMapping, array $mapping, array &$config): void
