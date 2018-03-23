ad09db2124670438d59eadebdddbfbc48e446f81
Roland Franssen <franssen.roland@gmail.com>
2018-03-23 18:55:49 +0100

    domain contracts

---
+++ src/Domain/Infra/Psr/DoctrineAwareClassContainer.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MsgPhp\Domain\Infra\Psr;
+
+use Doctrine\Common\Util\ClassUtils;
+use Psr\Container\ContainerInterface;
+
+final class DoctrineAwareClassContainer implements ContainerInterface
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function has($id)
+    {
+        return $this->container->has(ClassUtils::getRealClass($id));
+    }
+
+    public function get($id)
+    {
+        return $this->container->get(ClassUtils::getRealClass($id));
+    }
+}
