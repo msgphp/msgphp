d6f44b5bc2952f962f3b6d531404c1d8baba88ec
Roland Franssen <franssen.roland@gmail.com>
2018-04-08 16:16:01 +0200

    structure

--- src/Domain/Infra/DependencyInjection/BundleHelper.php
+++ src/Domain/Infra/DependencyInjection/BundleHelper.php
@@ -30,67 +30,10 @@ final class BundleHelper
             return;
         }
 
-        if (class_exists(ConsoleEvents::class)) {
-            $container->register(ConsoleInfra\Context\ClassContextFactory::class)
-                ->setPublic(false)
-                ->setAbstract(true)
-                ->setAutowired(true)
-                ->setArgument('$method', '__construct')
-                ->setArgument('$classMapping', '%msgphp.domain.class_mapping%');
-
-            $container->register(ConsoleInfra\Context\ClassContextElementFactory::class)
-                ->setPublic(false);
-
-            $container->setAlias(ConsoleInfra\Context\ClassContextElementFactoryInterface::class, new Alias(ConsoleInfra\Context\ClassContextElementFactory::class, false));
-
-            $container->register(ConsoleInfra\Event\MessageSubscriber::class)
-                ->setPublic(false)
-                ->addTag('kernel.event_listener', ['event' => ConsoleEvents::COMMAND, 'method' => 'onCommand'])
-                ->addTag('kernel.event_listener', ['event' => ConsoleEvents::TERMINATE, 'method' => 'onTerminate']);
-            if (interface_exists(MiddlewareInterface::class)) {
-                $container->register(MessengerInfra\ConsoleMessageSubscriberMiddleware::class)
-                    ->setPublic(false)
-                    ->setAutowired(true)
-                    ->addTag('message_bus_middleware');
-            }
-            if (interface_exists(MessageBusMiddleware::class)) {
-                $container->register(SimpleBusInfra\ConsoleMessageSubscriberMiddleware::class)
-                    ->setPublic(false)
-                    ->setAutowired(true)
-                    ->addTag('command_bus_middleware');
-            }
-        }
-
-        if (class_exists(DoctrineOrmVersion::class)) {
-            $container->addCompilerPass(new Compiler\DoctrineObjectFieldMappingPass());
-
-            $container->register(DoctrineInfra\Event\ObjectFieldMappingListener::class)
-                ->setPublic(false)
-                ->setArgument('$mapping', [])
-                ->addTag('doctrine.event_listener', ['event' => DoctrineOrmEvents::loadClassMetadata])
-                ->addTag('msgphp.domain.process_class_mapping', ['argument' => '$mapping']);
-
-            if (ContainerHelper::hasBundle($container, DoctrineBundle::class)) {
-                @mkdir($mappingDir = $container->getParameterBag()->resolveValue('%kernel.cache_dir%/msgphp/doctrine-mapping'), 0777, true);
-
-                $container->prependExtensionConfig('doctrine', ['orm' => [
-                    'hydrators' => [
-                        DoctrineInfra\Hydration\ScalarHydrator::NAME => DoctrineInfra\Hydration\ScalarHydrator::class,
-                        DoctrineInfra\Hydration\SingleScalarHydrator::NAME => DoctrineInfra\Hydration\SingleScalarHydrator::class,
-                    ],
-                    'mappings' => [
-                        'msgphp' => [
-                            'dir' => $mappingDir,
-                            'type' => 'xml',
-                            'prefix' => 'MsgPhp',
-                            'is_bundle' => false,
-                        ],
-                    ],
-                ]]);
-            }
-        }
-
         $container->addCompilerPass(new Compiler\ResolveDomainPass());
+
+        self::initConsole($container);
+        self::initDoctrineOrm($container);
 
         $initialized = true;
     }
@@ -109,6 +52,76 @@ final class BundleHelper
         }
 
         $initialized = true;
+    }
+
+    private static function initConsole(ContainerBuilder $container): void
+    {
+        if (!class_exists(ConsoleEvents::class)) {
+            return;
+        }
+
+        $container->register(ConsoleInfra\Context\ClassContextFactory::class)
+            ->setPublic(false)
+            ->setAbstract(true)
+            ->setAutowired(true)
+            ->setArgument('$method', '__construct')
+            ->setArgument('$classMapping', '%msgphp.domain.class_mapping%');
+
+        $container->register(ConsoleInfra\Context\ClassContextElementFactory::class)
+            ->setPublic(false);
+
+        $container->setAlias(ConsoleInfra\Context\ClassContextElementFactoryInterface::class, new Alias(ConsoleInfra\Context\ClassContextElementFactory::class, false));
+
+        $container->register(ConsoleInfra\Event\MessageSubscriber::class)
+            ->setPublic(false)
+            ->addTag('kernel.event_listener', ['event' => ConsoleEvents::COMMAND, 'method' => 'onCommand'])
+            ->addTag('kernel.event_listener', ['event' => ConsoleEvents::TERMINATE, 'method' => 'onTerminate']);
+
+        if (interface_exists(MiddlewareInterface::class)) {
+            $container->register(MessengerInfra\ConsoleMessageSubscriberMiddleware::class)
+                ->setPublic(false)
+                ->setAutowired(true)
+                ->addTag('message_bus_middleware');
+        }
+        if (interface_exists(MessageBusMiddleware::class)) {
+            $container->register(SimpleBusInfra\ConsoleMessageSubscriberMiddleware::class)
+                ->setPublic(false)
+                ->setAutowired(true)
+                ->addTag('command_bus_middleware');
+        }
+    }
+
+    private static function initDoctrineOrm(ContainerBuilder $container): void
+    {
+        if (!class_exists(DoctrineOrmVersion::class)) {
+            return;
+        }
+
+        $container->addCompilerPass(new Compiler\DoctrineObjectFieldMappingPass());
+
+        $container->register(DoctrineInfra\Event\ObjectFieldMappingListener::class)
+            ->setPublic(false)
+            ->setArgument('$mapping', [])
+            ->addTag('doctrine.event_listener', ['event' => DoctrineOrmEvents::loadClassMetadata]);
+
+        if (ContainerHelper::hasBundle($container, DoctrineBundle::class)) {
+            @mkdir($mappingDir = $container->getParameterBag()->resolveValue('%kernel.cache_dir%/msgphp/doctrine-mapping'), 0777, true);
+
+            $container->prependExtensionConfig('doctrine', ['orm' => [
+                'hydrators' => [
+                    DoctrineInfra\Hydration\ScalarHydrator::NAME => DoctrineInfra\Hydration\ScalarHydrator::class,
+                    DoctrineInfra\Hydration\SingleScalarHydrator::NAME => DoctrineInfra\Hydration\SingleScalarHydrator::class,
+                ],
+                'mappings' => [
+                    'msgphp' => [
+                        'dir' => $mappingDir,
+                        'type' => 'xml',
+                        'prefix' => 'MsgPhp',
+                        'is_bundle' => false,
+                    ],
+                ],
+            ]]);
+        }
     }
 
     private static function &getInitialized(Container $container, string $key)
