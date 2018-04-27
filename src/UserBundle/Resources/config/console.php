c4b147c2751fc81266b7bc5c12a26abb0153bfa3
Roland Franssen <franssen.roland@gmail.com>
2018-04-27 11:51:59 +0200

    bind console specific bus

--- src/UserBundle/Resources/config/console.php
+++ src/UserBundle/Resources/config/console.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use MsgPhp\Domain\Infra\Console;
+use MsgPhp\Domain\Message\DomainMessageBusInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return function (ContainerConfigurator $container): void {
@@ -11,6 +12,7 @@ return function (ContainerConfigurator $container): void {
             ->autowire()
             ->autoconfigure()
             ->private()
+            ->bind(DomainMessageBusInterface::class, Console\DomainMessageBus::class)
 
         ->load('MsgPhp\\User\\Infra\\Console\\Command\\', '%kernel.project_dir%/vendor/msgphp/user/Infra/Console/Command')
     ;
