7204b0210b6191f0dc2c05a37da8d3c2bf296f2c
Brian Buehrle <bbuehrle@thepisagroup.com>
2018-07-08 14:36:55 -0500

    Add link in profile to quickly logout

--- src/UserBundle/Resources/skeleton/template/profile.html.php
+++ src/UserBundle/Resources/skeleton/template/profile.html.php
@@ -13,6 +13,7 @@ return <<<TWIG
     <h1>Your Profile</h1>
 
     <p>Logged in as: <em>{{ ${userVariable}.current.${fieldName} }}</em></p>
+    <p><a href="{{ path('logout') }}">Logout</a></p>
 {% endblock %}
 
 TWIG;
